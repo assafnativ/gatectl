c0bb0f77d8f26983b0ce56ec78a85bd878d89ec8
Assaf Nativ <nativ.assaf@gmail.com>
2021-07-22 20:58:05 +0300

    Fine tuning

--- gatectl.py
+++ gatectl.py
@@ -277,7 +277,7 @@ class TelegramBot(object):
     def getMessages(self):
         messages = None
         try:
-            messages = self.bot.getUpdates(self.lastMsgId, timeout=1)
+            messages = self.bot.getUpdates(self.lastMsgId, timeout=0.7)
         except:
             return []
         result = []
